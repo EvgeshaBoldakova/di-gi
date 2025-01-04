3e5f94b3575bad0d5eaca4cb7ad7021ecc57bab8
Євгенія Болдакова <165957744+EvgeshaBoldakova@users.noreply.github.com>
2025-01-04 02:20:31 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
-  $('#block_2').owlCarousel({
+  $("#block_2").owlCarousel({
     items: 1,
     loop: true,
     nav: false,
     dots: true,
-    dotsContainer: '.pagination_one',
+    dotsContainer: ".pagination_one",
     startPosition: 1
   });
 
-  $('#block_8').owlCarousel({
+  $("#block_8").owlCarousel({
     items: 1,
     loop: true,
     nav: false,
     dots: true,
-    dotsContainer: '.pagination_two',
+    dotsContainer: ".pagination_two",
     startPosition: 1
   });
 
-  $('.pagination_one .circle').click(function () {
+  $(".pagination_one .circle").click(function () {
     let index = $(this).index();
-    $('#block_2').trigger('to.owl.carousel', [index, 300]);
+    $("#block_2").trigger("to.owl.carousel", [index, 300]);
   });
 
-  $('.pagination_two .circle1').click(function () {
+  $(".pagination_two .circle1").click(function () {
     let index = $(this).index();
-    $('#block_8').trigger('to.owl.carousel', [index, 300]);
+    $("#block_8").trigger("to.owl.carousel", [index, 300]);
   });
 
-  $('.roundabout_left').click(function () {
-    $('.owl-carousel').trigger('prev.owl.carousel');
+  $(".roundabout_left").click(function () {
+    $("#block_2").trigger("prev.owl.carousel");
   });
 
-  $('.roundabout_right').click(function () {
-    $('.owl-carousel').trigger('next.owl.carousel');
+  $(".roundabout_right").click(function () {
+    $("#block_2").trigger("next.owl.carousel");
   });
 });
